d12283882790b4e7aabc9e8607883ef8cf4e8f7a
danielvieiralopes <danielvieira.2007@hotmail.com>
2020-04-25 11:08:00 -0300

    .

--- 03-funcao/09-arrowFunciton.js
+++ 03-funcao/09-arrowFunciton.js
@@ -3,3 +3,14 @@ let comparaComThis = function (param){
 }
 
 comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThis(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind
