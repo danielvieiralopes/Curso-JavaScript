c3c7fb1fb654afa9e26c60ed87a1aee28611202a
danielvieiralopes <danielvieira.2007@hotmail.com>
2020-05-04 17:14:36 -0300

    m

--- 04-Exercicios/17.js
+++ 04-Exercicios/17.js
@@ -1,6 +1,39 @@
-function pagamento (plano){
+function pagamento (plano, salarioAtual){
     switch(plano){
-        
+        case 'A':
+            return (salarioAtual + (salarioAtual * 0.1))
+        case 'B':
+            return (salarioAtual + (salarioAtual * 0.15))
+        case 'C':
+            return (salarioAtual + (salarioAtual * 0.20))
+        default:
+            return 'Número fora do intervalo.'
+
     }
 
 }
+
+console.log(pagamento('A', 1000))
+console.log(pagamento('B', 1000))
+console.log(pagamento('C', 1000))
+console.log(pagamento('D', 1000))
+
+//correcao
+
+function calcularReajuste(plano, salarioAtual) {
+    switch (plano) {
+        case 'A':
+            return salarioAtual * 1.1
+        case 'B':
+            return salarioAtual * 1.15
+        case 'C':
+            return salarioAtual * 1.2
+        default:
+            return 'Plano inválido.'
+    }
+}
+
+console.log(calcularReajuste('A', 1000));
+console.log(calcularReajuste('B', 1000));
+console.log(calcularReajuste('C', 1000));
+console.log(calcularReajuste('D', 1000));
