9d1a051b0df64f7497ac4eacf9b1565948d4797f
Daniel Vieira <62304465+danielvieiralopes@users.noreply.github.com>
2020-06-24 01:34:30 -0300

    cont nodejs

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -1,24 +1,22 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
-// axios.get(url).then(response => {
-//     const funcionarios = response.data
-//     console.log(funcionarios)
-// })
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
 
 axios.get(url).then(response => {
     const funcionarios = response.data
+    console.log(funcionarios)
 
+    // mulher chinesa com menor salario?
+    const func = funcionarios 
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario)
 
-    
-    const apenasChina = funcionarios.pais == china
-
-     const resultado = funcionarios.filter(apenasChina)
-     console.log(resultado)
-
-
-    // console.log(funcionarios.filter(apenasF))
-    
-
-
+    console.log(func)
 })
