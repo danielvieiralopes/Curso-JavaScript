395ffd72eab90bb759acf96e30d71eed9b5eb492
Daniel Vieira <62304465+danielvieiralopes@users.noreply.github.com>
2020-06-24 23:48:31 -0300

    finalizando nodejs

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -8,7 +8,7 @@ const bancoDeDados = require('./bancoDeDados')
 app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/produtos', (req, res, next) => {
-    res.send(bancoDeDados.getProduto())
+    res.send(bancoDeDados.getProdutos())
 })
 
 app.get('/produtos/:id', (req, res, next) => {
@@ -20,6 +20,20 @@ app.post('/produtos', (req, res, next) => {
         nome: req.body.nome,
         preco: req.body.preco
     })
+    res.send(produto) //JSON
+}) 
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
     res.send(produto) //JSON
 })
 
